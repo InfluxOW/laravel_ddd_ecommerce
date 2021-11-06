8b8203fb31849484e105640b69bc9b6f4b59dc40
Nikita Krochak <krch@feip.co>
2021-11-06 16:50:23 +1000

    Detach Laravel Sail dependency

--- make-compose-ci.mk
+++ make-compose-ci.mk
@@ -1,9 +1,12 @@
-sail := ./vendor/bin/sail
+# Works only for project root
+
+sail_dir := $(shell pwd)/tools/laravel/sail
+sail := $(sail_dir)/vendor/bin/sail
 
 setup:
 	docker run --rm \
 		-u "$(shell id -u):$(shell id -g)" \
-		-v $(shell pwd):/var/www/html \
+		-v $(sail_dir):/var/www/html \
 		-w /var/www/html \
 		laravelsail/php80-composer:latest \
 		composer install --ignore-platform-reqs
