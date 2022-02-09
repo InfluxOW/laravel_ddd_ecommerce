030242451a5b0d3675b6470b23a25dc86a24c3ea
Nikita Krochak <krch@feip.co>
2022-02-09 22:41:06 +1000

    Fix fallback route

--- app/Domains/Generic/Providers/RouteServiceProvider.php
+++ app/Domains/Generic/Providers/RouteServiceProvider.php
@@ -8,8 +8,15 @@ use Illuminate\Routing\Router;
 
 final class RouteServiceProvider extends BaseRouteServiceProvider
 {
+    public function map(Router $router): void
+    {
+        parent::map($router);
+
+        $router->fallback(static fn (): RedirectResponse => redirect()->to(config('l5-swagger.documentations.default.routes.api')));
+    }
+
     protected function mapApiRoutes(Router $router): void
     {
-        $router->fallback(static fn (): RedirectResponse => redirect()->to(config('l5-swagger.documentations.default.routes.api')));
+        //
     }
 }
