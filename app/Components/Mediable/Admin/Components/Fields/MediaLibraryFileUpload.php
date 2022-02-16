a03804b94b540ad4829fdddff7be5f3c5ffa1109
Nikita Krochak <krch@feip.co>
2022-02-16 23:27:22 +1000

    Temporarily fix broken admin images

--- app/Components/Mediable/Admin/Components/Fields/MediaLibraryFileUpload.php
+++ app/Components/Mediable/Admin/Components/Fields/MediaLibraryFileUpload.php
@@ -2,7 +2,9 @@
 
 namespace App\Components\Mediable\Admin\Components\Fields;
 
+use Carbon\Carbon;
 use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 final class MediaLibraryFileUpload extends SpatieMediaLibraryFileUpload
 {
@@ -13,5 +15,18 @@ final class MediaLibraryFileUpload extends SpatieMediaLibraryFileUpload
         $this
             ->disk(config('filesystems.default'))
             ->visibility('private');
+
+        // Temporary fix until it's fixed in Filament
+        $this->getUploadedFileUrlUsing(function (SpatieMediaLibraryFileUpload $component, string $file): ?string {
+            if ($component->getRecord() === null) {
+                return null;
+            }
+
+            $mediaClass = config('media-library.media_model', Media::class);
+            /** @var ?Media $media */
+            $media = $mediaClass::findByUuid($file);
+
+            return ($component->getVisibility() === 'private') ? $media?->getTemporaryUrl(Carbon::now()->addMinutes(5)) : $media?->getUrl();
+        });
     }
 }
