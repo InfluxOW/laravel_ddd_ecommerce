38415e913185f834c7879fd360e2562a500c214c
Nikita Krochak <krochak@in4security.com>
2022-07-29 01:43:46 +1000

    Update Admin model docs

--- app/Domains/Admin/Models/Admin.php
+++ app/Domains/Admin/Models/Admin.php
@@ -21,6 +21,8 @@ use Illuminate\Notifications\Notifiable;
  * @property string|null                     $remember_token
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|LoginHistory[] $loginHistory
+ * @property-read int|null $login_history_count
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
  * @property-read int|null $notifications_count
  *
