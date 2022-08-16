f9826250c9bf575f01dbd3a4e3c644f7ef609a63
Nikita Krochak <krochak@in4security.com>
2022-08-17 00:49:40 +1000

    Refactor app:refresh command

--- app/Domains/Generic/Console/Commands/RefreshApplicationCommand.php
+++ app/Domains/Generic/Console/Commands/RefreshApplicationCommand.php
@@ -3,7 +3,7 @@
 namespace App\Domains\Generic\Console\Commands;
 
 use App\Domains\Generic\Database\Seeders\DatabaseSeeder;
-use Elastic\Transport\Exception\RuntimeException;
+use Elastic\Transport\Exception\TransportException;
 use Illuminate\Console\Command;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Redis;
@@ -20,22 +20,57 @@ final class RefreshApplicationCommand extends Command
             return self::INVALID;
         }
 
+        $this->restartHorizon();
+        $this->clearMedia();
+        $this->refreshDatabase();
+        $this->clearCache();
+        $this->fillDatabase();
+        $this->refreshElastic();
+        $this->fillElastic();
+
+        return self::SUCCESS;
+    }
+
+    private function restartHorizon(): void
+    {
         $this->call('horizon:terminate', ['--wait' => true]);
+    }
+
+    private function clearMedia(): void
+    {
         try {
             $this->call('media-library:clear');
         } catch (QueryException) {
         }
+    }
+
+    private function refreshDatabase(): void
+    {
         $this->call('migrate:fresh');
+    }
+
+    private function clearCache(): void
+    {
         Redis::flushall();
+    }
+
+    private function fillDatabase(): void
+    {
         $this->call('db:seed', ['--class' => DatabaseSeeder::class]);
+    }
+
+    private function refreshElastic(): void
+    {
         try {
             $this->call('elastic:migrate:refresh');
-        } catch (RuntimeException) {
+        } catch (TransportException) {
         }
+    }
+
+    private function fillElastic(): void
+    {
         foreach (config('scout.models') as $model) {
             $this->call('scout:import', ['model' => $model]);
         }
-
-        return self::SUCCESS;
     }
 }
