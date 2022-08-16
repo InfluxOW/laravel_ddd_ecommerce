1c3d61fd9adc21ba54efd0fabb98033a7a9698d7
Nikita Krochak <krochak@in4security.com>
2022-08-17 00:44:20 +1000

    Set Elasticsearch hosts authentication

--- config/elastic.client.php
+++ config/elastic.client.php
@@ -1,12 +1,24 @@
 <?php
 
-return [
+$elasticUrl = env('ELASTICSEARCH_URL') ?? 'localhost:9200';
+
+$config = [
     'default' => env('ELASTIC_CONNECTION', 'default'),
     'connections' => [
         'default' => [
             'hosts' => [
-                env('ELASTICSEARCH_URL', 'localhost:9200'),
+                $elasticUrl,
             ],
         ],
     ],
 ];
+
+$parsedElasticUrl = parse_url($elasticUrl);
+$elasticUsername = $parsedElasticUrl['user'] ?? null;
+$elasticPassword = $parsedElasticUrl['pass'] ?? null;
+
+if (isset($elasticUsername, $elasticPassword)) {
+    $config['connections']['default']['basicAuthentication'] = [$elasticUsername, $elasticPassword];
+}
+
+return $config;
